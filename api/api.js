839050551b8d70961b8ab3ef17765c1037c41c79
Yann Helleboid <yann.helleboid@orange.com>
2025-02-20 10:32:28 +0100

    creation

--- api/api.js
+++ api/api.js
@@ -67,7 +67,7 @@ async function start(fastify)
   const path = require('node:path');
   fastify.register(require('@fastify/static'),
   {
-    root: path.join(__dirname, 'public'),
+    root: path.join(__dirname, '../public'),
     prefix: '/'
   });
   registerfastifyroutes(fastify);
